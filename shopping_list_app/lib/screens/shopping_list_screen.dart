92f6b74639d8adfcd1987af327708e1e8a8957d6
saladve <sachi.akutsu0111@gmail.com>
2025-11-26 16:38:57 +0900

    11/26/25 コミット

--- shopping_list_app/lib/screens/shopping_list_screen.dart
+++ shopping_list_app/lib/screens/shopping_list_screen.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import '../models/shopping_item.dart';
+import '../screens/new_item_screen.dart';
 
 class ShoppingListScreen extends StatefulWidget {
   const ShoppingListScreen({super.key});
 
-  // Stateオブジェクトを作成するメソッド
   @override
   State<ShoppingListScreen> createState() => _ShoppingListScreenState();
 }
 
 class _ShoppingListScreenState extends State<ShoppingListScreen> {
-//ダミーデータ
+// ダミーデータ
   final List<ShoppingItem> _shoppingList = [
     const ShoppingItem(name: '牛乳', quantity: 1, isCompleted: false),
     const ShoppingItem(name: 'パン', quantity: 2, isCompleted: true),
@@ -18,14 +18,35 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
     const ShoppingItem(name: 'トマト'),
   ];
 
-  // チェックボックスの状態を切り替えるメソッド（次のステップで利用）
+  // チェックボックスの状態を切り替えるメソッド
   void _toggleCompletion(int index) {
-    // setStateブロック内でデータを変更すると、UIが自動的に再描画されます
     setState(() {
       final currentItem = _shoppingList[index];
 
       _shoppingList[index] = currentItem.copyWith(
         isCompleted: !currentItem.isCompleted,
+      );
+    });
+  }
+
+  void _addItem(BuildContext context) async {
+    final result = await Navigator.of(context).push<String>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItemScreen(),
+      ),
+    );
+
+    if (result == null) {
+      return;
+    }
+
+    setState(() {
+      _shoppingList.add(
+        ShoppingItem(
+          name: result, // NewItemScreenから返されたアイテム名を使用
+          quantity: 1, // 新規追加アイテムのデフォルト数量
+          isCompleted: false,
+        ),
       );
     });
   }
@@ -67,17 +88,10 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
         },
       ),
 
-      // 画面右下のフローティングアクションボタン（アイテム追加用）
+      // 追加ボタン
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            _shoppingList.add(
-              ShoppingItem(
-                name: '新規アイテム ${DateTime.now().second}', // 時刻でユニークな名前に
-                quantity: 1,
-              ),
-            );
-          });
+          _addItem(context);
         },
         child: const Icon(Icons.add),
       ),
